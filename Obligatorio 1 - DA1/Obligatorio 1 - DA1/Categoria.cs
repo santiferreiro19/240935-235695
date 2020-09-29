915fea647abe4e2657bd497a8d1e576f2401f598
Martin Robatto <martinrobatto8@gmail.com>
2020-09-29 18:17:34 -0300

    Creacion CategoriaTest + implementacion

---
+++ Obligatorio 1 - DA1/Obligatorio 1 - DA1/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio_1___DA1
+{
+    public class Categoria
+    {
+        public String Nombre { set; get; }
+        List<String> ListaPalabras;
+
+        public Categoria(String unNombre,  List<String> unasPalabras) {
+            this.Nombre = unNombre;
+            this.ListaPalabras = unasPalabras;
+        }
+        public Categoria(String unNombre)
+        {
+            this.Nombre = unNombre;
+            this.ListaPalabras = new List<string>();
+        }
+
+    }
+}
